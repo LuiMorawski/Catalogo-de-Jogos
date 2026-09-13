9b44bac8a6f9843ec6dfbb18afac75441f825feb
LuizMorawski <lemorawski@gmail.com>
2026-09-13 15:55:28 -0300

    Tinha dado errado antes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿
 class Program
 {
-    
+    static CatalogoServico catalogo = new CatalogoServico();
     public static void Main(string[] args)
     {
 
@@ -38,6 +38,7 @@ while(true)
            break;
 
            case 5:
+           AvaliarJogoMenu();
            break;
 
            case 6:
@@ -56,4 +57,50 @@ Console.ForegroundColor = ConsoleColor.White;
     } 
      }
 } 
+
+static void AvaliarJogoMenu()
+    {
+        try
+        {
+            Console.WriteLine("Digite o ID do jogo que deseja avaliar:");
+            if (!int.TryParse(Console.ReadLine(), out int idJogo))
+            {
+                MostrarErro("Id invalido.");
+                return;
+            }
+ 
+            Console.WriteLine("Digite seu nome:");
+            string autor = Console.ReadLine();
+ 
+            Console.WriteLine("Digite sua nota (0 a 10):");
+            if (!int.TryParse(Console.ReadLine(), out int nota))
+            {
+                MostrarErro("Nota invalida.");
+                return;
+            }
+ 
+            Console.WriteLine("Digite seu comentario (opcional):");
+            string comentario = Console.ReadLine();
+ 
+            var avaliacao = catalogo.AvaliarJogo(idJogo, autor, nota, comentario);
+ 
+            MostrarSucesso($"Avaliação registrada para \"{avaliacao.Jogo.Titulo}\"!");
+        }
+        catch (Exception ex)
+        {
+            MostrarErro(ex.Message);
+        }
+    }
+  static void MostrarErro(string mensagem)
+  {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(mensagem);
+    Console.ForegroundColor = ConsoleColor.White;
+  }
+  static void MostrarSucesso(string mensagem)
+  {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(mensagem);
+    Console.ForegroundColor = ConsoleColor.White;
+  }
 }
