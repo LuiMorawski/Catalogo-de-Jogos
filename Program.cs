85919c1f1d35a9fbf7a37f5d730e387e9f16c976
Erick <erickmendes448@gmail.com>
2026-09-13 15:22:28 -0300

    Implementa cadastro, listagem, busca e remoção de jogos

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+
 class Program
 {
-    
+    static CatalogoServices catalogoServices = new CatalogoServices();
     public static void Main(string[] args)
     {
 
+
 while(true)
         {
+          // limpa o console
+Console.Clear();          
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("╔═══════════════════════════╗");
         Console.WriteLine("║           MENU            ║");
@@ -26,15 +32,44 @@ while(true)
       int Op = int.Parse(System.Console.ReadLine());
         switch(Op){ 
            case 1:
+           System.Console.WriteLine("Digite o titulo do Jogo: ");
+           string titulo = Console.ReadLine();
+
+           System.Console.WriteLine("Digite o ano do Jogo: ");
+           int ano = int.Parse(System.Console.ReadLine());
+          // Exemplo 
+           Genero genero = new Genero("ACao");
+
+           catalogoServices.CadastrarJogo(titulo,ano,genero);
+           Console.ForegroundColor = ConsoleColor.Green;
+           System.Console.WriteLine("Jogo Cadastrado com Sucesso!");
+           Console.ForegroundColor = ConsoleColor.White;
+           Console.WriteLine("\nPressione qualquer tecla para continuar...");
+           Console.ReadKey();
            break;
 
            case 2:
+           System.Console.WriteLine("Jogos Disponiveis:");
+           catalogoServices.ListarJogos();
+           Console.WriteLine("\nPressione qualquer tecla para continuar...");
+           Console.ReadKey();
            break;
 
            case 3:
+           System.Console.WriteLine("Digite o título (ou parte dele) que deseja buscar: ");
+           string busca = Console.ReadLine();
+           catalogoServices.BuscarJogo(busca);
+           Console.WriteLine("\nPressione qualquer tecla para continuar...");
+           Console.ReadKey();
            break;
 
            case 4:
+           catalogoServices.ListarJogos();
+           System.Console.WriteLine("Digite o id do Jogo que quer Remover: ");
+           int IdJogoRemover = int.Parse(System.Console.ReadLine());
+           catalogoServices.RemoverJogo(IdJogoRemover);
+           Console.WriteLine("\nPressione qualquer tecla para continuar...");
+           Console.ReadKey();
            break;
 
            case 5:
